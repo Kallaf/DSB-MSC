8c2be299e31d920c4745d4976a86b7f1ae3fb5cc
Kallaf <kalaf1997@yahoo.com>
2019-08-22 20:04:42 +0200

    Read the attached audio file which has the sampling frequency 48kHz, and plot the spectrum of the signal

--- main.m
+++ main.m
@@ -1,5 +1,32 @@
-[xn fs] = audioread('tone.wav');
-nf=1024; %number of point in DTFT
-Y = fft(xn,nf);
-f = fs/2*linspace(0,1,nf/2+1);
-plot(f,abs(Y(1:nf/2+1)));
+% 1.1) Read the attached audio file which has the sampling frequency 48kHz
+%-------------------------------------------------------------------------
+
+[signal,Fs] = audioread('tone.wav'); %where fs is the sampling frequency
+
+
+% 1.2) plot the spectrum of the signal
+%-------------------------------------
+
+X = signal(:,1);		% Removing the second channel
+T = 1/Fs;				% Sampling period 
+L = length(X);			% Length of signal
+t = (0:L-1)*T; 			% Time vector
+
+
+% Compute the Fourier transform of the signal.
+Y = fft(X);
+
+% Compute the two-sided spectrum P2. Then compute the single-sided spectrum P1 based on P2 and the even-valued signal length L.
+P2 = abs(Y/L);
+P1 = P2(1:L/2+1);
+P1(2:end-1) = 2*P1(2:end-1);
+
+% Define the frequency domain f and plot the single-sided amplitude spectrum P1.
+f = Fs*(0:(L/2))/L;
+plot(f,P1) 
+title('Single-Sided Amplitude Spectrum of X(t)')
+xlabel('f (Hz)')
+ylabel('|P1(f)|')
+
+
+%lowpass(xn,4,fs)
