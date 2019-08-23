f95e1ba2422207d0789175fa92ff7ddf5e0d167a
Kallaf <kalaf1997@yahoo.com>
2019-08-24 00:07:19 +0200

    Sound the filtered audio signal

--- main.m
+++ main.m
@@ -7,44 +7,33 @@
 % 1.2)Plot the spectrum of the signal
 %-------------------------------------
 
-X = signal(:,1);		% Removing the second channel
+y = signal(:,1);		% Removing the second channel
 T = 1/Fs;				% Sampling period 
-L = length(X);			% Length of signal
-t = (0:L-1)*T; 			% Time vector
+L = length(y);			% Length of signal
 
 % Compute the Fourier transform of the signal.
-Y = fft(X);
+Y = fft(y);
 
-% Compute the two-sided spectrum P2. Then compute the single-sided spectrum P1 based on P2 and the even-valued signal length L.
-P2 = abs(Y/L);
-P1 = P2(1:L/2+1);
-P1(2:end-1) = 2*P1(2:end-1);
-
-
-
-% Define the frequency domain f and plot the single-sided amplitude spectrum P1.
-f = Fs*(0:(L/2))/L;
-plot(f,P1)
-title('Single-Sided Amplitude Spectrum of X(t)')
+% Define the frequency domain f and plot the double-sided amplitude spectrum.
+f = Fs*(0:L-1);
+plot(f,abs(Y))
+title('Double-Sided Amplitude Spectrum of signal')
 xlabel('f (Hz)')
-ylabel('|P1(f)|')
+ylabel('P (f)')
 
-% Use an ideal filter to remove all the frequencies greater than 4KHZ
+% 2)Use an ideal filter to remove all the frequencies greater than 4KHZ
+%----------------------------------------------------------------------
 
-cutoffLength = 1;
-
-for i = 1:length(f)
-	if f(i) >= 4000
-		cutoffLength = i-1;
-		break;
-	end
-end
-
-% 2)Use an ideal filter to remove all frequencies greater than 4KHZ
-
-P1 = P1(1:cutoffLength);
-f = f(1:cutoffLength);
+cut_off = 4e3/Fs/2;
+order = 32;
+h = fir1(order,cut_off);
 
 % 3)Obtain the filtered signal in both frequency and time domain.
+%----------------------------------------------------------------
 
-y = ifft(P1);
+con = conv(y,h);
+
+% 4)Sound the filtered audio signal
+%----------------------------------
+sound(con, Fs, 16);
+
